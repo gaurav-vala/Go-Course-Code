a7aa2ee39be67e1fae0e54a685719a147c3d28d3
Gaurav <gauravvala492@gmail.com>
2024-09-25 08:37:57 +0530

    Go Practice 1 Profit Calculator Solved

--- Go-Basics/investment_calculator.go
+++ Go-Basics/investment_calculator.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello")
+	const inflationRate = 2.5
+	var investmentAmount float64 = 1000
+	var years float64
+	expectedReturn := 5.5
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Investment Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Returns: ")
+	fmt.Scan(&expectedReturn)
+
+	futureValue := (investmentAmount) * math.Pow(1+expectedReturn/100, float64(years))
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
---
+++ Go-Practice-1-Profit-Calculator/profit-calculator.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var tax_rate float64
+
+	fmt.Print("Enter your Renvenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Enter your Expenses: ")
+	fmt.Scan(&expenses)
+	fmt.Print("Enter the Tax rate: ")
+	fmt.Scan(&tax_rate)
+
+	ebt := revenue - expenses
+	var profit float64 = ebt * (1 - (tax_rate / 100))
+
+	ratio := ebt / profit
+
+	fmt.Println(ebt)
+	fmt.Println(profit)
+	fmt.Println(ratio)
+
+}
